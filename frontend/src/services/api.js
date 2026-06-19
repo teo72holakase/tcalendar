98e023f2b61dd494e4a254f9ba39342410f3828d
teo72holakase <teo72holakase@gmail.com>
2026-06-19 17:54:03 -0300

    1.4.5 - Api Fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tcalendar.onrender.com/api',
+  baseURL: 'https://tcalendar.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
