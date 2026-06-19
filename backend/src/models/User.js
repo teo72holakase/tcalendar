8d494701c9d4a2e430569d93bb18e4b88391c87f
teo72holakase <teo72holakase@gmail.com>
2026-06-19 17:05:12 -0300

    1.4 - Controller Fix

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,17 +5,27 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
     trim: true,
+    lowercase: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
-  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
+  groups: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Group'
+  }],
   createdAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
