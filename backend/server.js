97a7683e04950d6f0301a9b2ce50fd0833531ec4
teo72holakase <teo72holakase@gmail.com>
2026-06-19 16:37:15 -0300

    1.3 - Webfix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true,
 }));
 
